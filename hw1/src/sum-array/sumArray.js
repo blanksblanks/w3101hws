ccd2d1366caf1f7c825ebc9cc48d3e7e57d0b87d
Nina Baculinao <nb2406@columbia.edu>
2015-01-26 11:37:52 -0500

    feat(create functional sumarray)

--- hw1/src/sum-array/sumArray.js
+++ hw1/src/sum-array/sumArray.js
@@ -4,7 +4,19 @@
   var sumArray = function sumArray(values) {
     // IMPLEMENT YOUR SOLUTION HERE!
     // MAKE SURE TO USE REDUCE!
-  };
+    return values.reduce(sum, 0);
+  }; 
+
+function sum(prev, curr) {
+	return prev + curr;
+}
+
+// [1, 2, 3] prev = 1, curr = 2
+// return 3
+// prev = 3
+
+// var sum = [1, 2, 3].reduce( function(total, num){ return total + num }, 0)
+
 
   module.exports = sumArray; // DON'T CHANGE THIS
 })();
