f31d9b01fcb399ed2d2f1fa4a7d00252dfa4180d
Nina Baculinao <nb2406@columbia.edu>
2015-01-27 10:03:37 -0500

    refactor(BallFollower.js): clean up comments and console logs'

--- hw1/src/follow-the-ball/BallFollower.js
+++ hw1/src/follow-the-ball/BallFollower.js
@@ -3,60 +3,39 @@
 
   var BallFollower = function BallFollower(start, swaps) {
     // IMPLEMENT YOUR SOLUTION WITHIN THIS FUNCTION
+
+    console.log('Parameters: ' + start + ", [" + swaps + ']');
+
     var end = start;
     if (Object.prototype.toString.call(swaps) === '[object Array]'){
 	  	swaps.forEach( // had to remove end =
 	  		function followBall(el) {
+	  			console.log('ball in: ' + end + ' bfr swap: [' + el[0] + ',' + el[1] + ']');
 	  			if (el[0] == end) {
 	  				end = el[1];
 	  			} else if (el[1] == end) {
 	  				end = el[0];
 	  			}
-	  			// console.log("Inside for each: " + end);
+	  			console.log('ball in: ' + end + ' aft swap: [' + el[0] + ',' + el[1] + ']');
 	  		});
   	}
 
-  	// console.log("Outside for each: " + end);
 	function swap(){
+		console.log('final ball position: ' + end);
 	  	return end;
 	}
 
-	// BallFollower.swap === function swap()
-  	// BallFollower.start === end
+	// because specs expect values for follower.start and follower.swap()
+	// BallFollower.swap = function swap()
+  	// BallFollower.start = end
   	return {
   		swap : swap,
   		start : end
   	}
-
-  	// var ballPosition = start;
-  	// swaps.forEach(function(el, idx, arr){
-  	// 	// var orig 	= el[0];
-  	// 	// el[0] 		= el[1];
-  	// 	// el[1] 		= orig;
-  	// 	// console.log(ballPosition, el);
-  	// 	if (el.indexOf(ballPosition) !== -1){
-  	// 		ballPosition = el[];
-  	// 		console.log(ballPosition);
-  	// 	}
-  	// });
-  	// start = console.log('Parameters: ' + start + ", " + swaps);
-  	// function followBall(start, cups) { // element, index, array
-  	//   console.log(start + ' ' + cups);
-  	//   if (start == cups[0]) {
-  	//     start = cups[1];
-  	//   } else if (start == cups[1]) {
-  	//     start = cups[0];
-  	//   }
-  	//   console.log('new: ' + start);
-  	//   return start;
-    //  }
-    //  return swaps.forEach(followBall(start, swaps), 0);
+  	
   };
 
-  // console.log(BallFollower(2, [[0, 2], [1, 0]]));
-
-  // start = 2
-  // swap1 = 0 and 2 switch
-
+  console.log(BallFollower(2, [[0, 2], [1, 0]]));
+  
   module.exports = BallFollower; // DON'T ALTER THIS
 })();
