8ff0fa91658d7219af5f30c51c3a1793df297717
Nina Baculinao <nb2406@columbia.edu>
2015-01-27 10:04:48 -0500

    refactor(sumArray.js): comment out console logs

--- hw1/src/sum-array/sumArray.js
+++ hw1/src/sum-array/sumArray.js
@@ -4,20 +4,20 @@
   var sumArray = function sumArray(values) {
     // IMPLEMENT YOUR SOLUTION HERE!
     // MAKE SURE TO USE REDUCE!
-    console.log('HEY WE GOT VALUES: ');
-    console.log(values);
+    // console.log('HEY WE GOT VALUES: ');
+    // console.log(values);
     return values.reduce(sum); // (sum, 0) not necessary
     // place function inside scope of var sumArray
 
     function sum(prev, curr) {
     	console.log(prev + '+' + curr);
-		return prev + curr;
-	}
+		  return prev + curr;
+	  }
 
   }; 
 
   // run node src/sum-array/sumArray.js to test console output
-  console.log(sumArray([11, 2, 3]));
+  // console.log(sumArray([11, 2, 3]));
 
   // Ex: var sum = [11, 2, 3].reduce( function(total, num){ return total + num }, 0)
   // HEY WE GOT VALUES: 
