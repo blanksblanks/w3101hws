51a8f9a7c0e38d03ae6d2ec4b7aa696c6bc11029
Nina Baculinao <nb2406@columbia.edu>
2015-01-27 10:16:56 -0500

    refactor(BallFollower.js): change == to expected ===, add semicolon to return of object literal

--- hw1/src/follow-the-ball/BallFollower.js
+++ hw1/src/follow-the-ball/BallFollower.js
@@ -4,16 +4,16 @@
   var BallFollower = function BallFollower(start, swaps) {
     // IMPLEMENT YOUR SOLUTION WITHIN THIS FUNCTION
 
-    console.log('Parameters: ' + start + ", [" + swaps + ']');
+    console.log('Parameters: ' + start + ', [' + swaps + ']');
 
     var end = start;
     if (Object.prototype.toString.call(swaps) === '[object Array]'){
 	  	swaps.forEach( // had to remove end =
 	  		function followBall(el) {
 	  			// console.log('ball in: ' + end + ' bfr swap: [' + el[0] + ',' + el[1] + ']');
-	  			if (el[0] == end) {
+	  			if (el[0] === end) {
 	  				end = el[1];
-	  			} else if (el[1] == end) {
+	  			} else if (el[1] === end) {
 	  				end = el[0];
 	  			}
 	  			// console.log('ball in: ' + end + ' aft swap: [' + el[0] + ',' + el[1] + ']');
@@ -24,14 +24,12 @@
 		// console.log('final ball position: ' + end);
 	  	return end;
 	}
-
+	
 	// because specs expect values for follower.start and follower.swap()
-	// BallFollower.swap = function swap()
-  	// BallFollower.start = end
-  	return {
+  	return { // so we return a new Object literal {};
   		swap : swap,
   		start : end
-  	}
+  	};
 
   };
 
