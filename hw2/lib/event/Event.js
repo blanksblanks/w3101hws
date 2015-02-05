0d845e80a1ac93ec3b062dc53d3925e8b199672c
Nina Baculinao <nb2406@columbia.edu>
2015-02-05 13:20:31 -0500

    refactor(Event.js): change output line order

--- hw2/lib/event/Event.js
+++ hw2/lib/event/Event.js
@@ -34,12 +34,12 @@
   var event = new Event();
   var someFunction = function (a, b) { return a + b; };
 
+  console.log("Event prototype: " + Object.prototype.toString.call(Event));
+  console.log("typeof Event: " + typeof Event);
+
   event.subscribe(someFunction);
   event.emit(1, 2); // someFunction will be called with these args
   event.unsubscribe(someFunction); // some function no longer subscribed
-
-  console.log("Event prototype: " + Object.prototype.toString.call(Event));
-  console.log("typeof Event: " + typeof Event);
 
   module.exports = Event;
 
