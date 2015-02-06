0f718716d818fc90be2bc65ceea62fe92094820e
Nina Baculinao <nb2406@columbia.edu>
2015-02-06 12:38:20 -0500

    refactor(BallFollower.js): remove commented-out alternate implementation, can also refer back here

--- hw1/src/follow-the-ball/BallFollower.js
+++ hw1/src/follow-the-ball/BallFollower.js
@@ -2,7 +2,6 @@
   'use strict';
 
   var BallFollower = function BallFollower(start, swaps) {
-    // IMPLEMENT YOUR SOLUTION WITHIN THIS FUNCTION
     this.start = start; // this is ONLY instantiated when we create new object
     this.swap = function swap() {
       if (Object.prototype.toString.call(swaps) === '[object Array]') {
@@ -22,38 +21,6 @@
       }
     };
   };
-
-  // Original implementation:
-  //
-  // var BallFollower = function BallFollower(start, swaps) {
-  //   // console.log('Parameters: ' + start + ', [' + swaps + ']');
-
-  //   var end = start;
-  //   if (Object.prototype.toString.call(swaps) === '[object Array]') {
-  //     swaps.forEach( // had to remove end =
-  //       function followBall(el) {
-  //         // console.log('ball in: ' + end + ' bfr swap: [' + el[0] + ',' + el[1] + ']');
-  //         if (el[0] === end) {
-  //           end = el[1];
-  //         } else if (el[1] === end) {
-  //           end = el[0];
-  //         }
-  //         // console.log('ball in: ' + end + ' aft swap: [' + el[0] + ',' + el[1] + ']');
-  //       });
-  //   }
-
-  // function swap() {
-  //   // console.log('final ball position: ' + end);
-  //     return end;
-  // }
-
-  // // because specs expect values for follower.start and follower.swap()
-  //   return { // so we return a new Object literal {};
-  //     swap : swap,
-  //     start : end
-  //   };
-
-  // };
 
   // test: // need the new when you return a function with this property
   // console.log(new BallFollower(2, [[0, 2], [1, 0]]));
