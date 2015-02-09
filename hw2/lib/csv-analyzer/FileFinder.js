ade07682439cf3e55050e1ca5dbe551d5f7310cd
Nina Baculinao <nb2406@columbia.edu>
2015-02-08 23:50:36 -0500

    feat(FileFinder.js): find filetype in a dir and return text, passes all tests

--- hw2/lib/csv-analyzer/FileFinder.js
+++ hw2/lib/csv-analyzer/FileFinder.js
@@ -1,4 +1,55 @@
-(function () {
+(function() {
   'use strict';
+
+  var fs = require('fs');
+
+  function FileFinder(pathToDirectory, fileType, fx) {
+    var stop = false;
+    fs.readdir(pathToDirectory, function(err, data) {
+      if (err) {
+        callbackError(err);
+      } else {
+        data.forEach(function(el, idx) {
+          if (data[idx].indexOf(fileType) !== -1) {
+            returnFileText(data[idx]); // async call
+            stop = true; //  w/o this callbackError returns before file has been returned
+          } else if (!stop && idx === data.length -1 && el.indexOf(fileType) === -1) {
+            callbackError('Unable to find ' + fileType + ' file');
+          }
+        });
+      }
+    });
+
+    function returnFileText(file) {
+      var fileToRead = pathToDirectory + file;
+
+      fs.readFile(fileToRead, function(err, data) {
+        if (err) {
+          callbackError(err);
+        } else {
+          fx(null, data.toString());
+        }
+      });
+    }
+
+    function callbackError(err) {
+      fx(err, null);
+    }
+
+  };
+
+
+  // FileFinder('./', 'txt', function (err, data));
+  // FileFinder(process.cwd() + '/', 'csv', function(err, data) {
+    // if (err) {
+    //   console.error('llll', err);
+    //   // return done();
+    // }
+    // var csvfile = data;
+    // console.log(csvfile);
+  //   // done();
+  // });
+
+  module.exports = FileFinder;
 
 })();
