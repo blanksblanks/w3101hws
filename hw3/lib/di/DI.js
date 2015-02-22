f5e2fd7717dfce4a539eaf6ee796aa935ca87019
Nina Baculinao <nb2406@columbia.edu>
2015-02-22 15:11:48 -0500

    test(di): 16/18 tests passing

--- hw3/lib/di/DI.js
+++ hw3/lib/di/DI.js
@@ -1,6 +1,55 @@
 (function () {
   'use strict';
 
-  module.exports = {};
+  var DI = function DI(name, dependencies) {
+    this.name = name;
+    this.dependencies = dependencies;
+    this.registeredFuncs = {};
+  };
 
+  DI.prototype.register = function(name, func) {
+    this.registeredFuncs[name] = func;
+  };
+
+  DI.prototype.inject = function(func) {
+    var funcString = func.toString();
+    var matches = funcString.match(/^function\s+?.*?\((.*?)\)/);
+    var argsNames = matches[1].split(',').map(function (arg) { return arg.trim(); });
+    var dependencies = this.dependencies;
+    var self = this;
+
+    // console.log('fs', funcString);
+    // console.log('ms', matches);
+    // console.log('aN', argsNames);
+    // console.log('ds', dependencies);
+
+    return function () {
+      var cachedFuncs = [];
+
+      argsNames.map(function(el) {
+        if (el) {
+          cachedFuncs.push(self.registeredFuncs[el]);
+        }
+      });
+
+      return func.apply(this, cachedFuncs); ///.apply(this, args);
+    };
+  };
+
+  module.exports = {
+    modules: {},
+    module: function (name, dependencies) {
+      if (arguments.length === 1) {
+        for (var i = 0; i < this.modules.length; i++) {
+          if (this.modules[i].name === name) {
+            return this.module[i];
+          }
+        }
+        // throw 'DI', name, 'is not available';
+      } else {
+        var module = new DI(name, dependencies);
+        return module;
+      }
+    }
+  };
 })();
