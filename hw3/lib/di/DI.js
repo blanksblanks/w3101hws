69244477488e86dacdfe70eaa7123df6ff89214a
Nina Baculinao <nb2406@columbia.edu>
2015-02-22 20:41:34 -0500

    refactor(di): refactor all code to revealing module pattern

--- hw3/lib/di/DI.js
+++ hw3/lib/di/DI.js
@@ -1,40 +1,54 @@
 (function () {
   'use strict';
 
-  var DI = function DI(name, dependencies) {
-    this.name = name;
-    this.dependencies = dependencies;
-    this.registeredFuncs = {};
-  };
-
-  DI.prototype.register = function(name, func) {
-    this.registeredFuncs[name] = func;
-  };
-
-  DI.prototype.inject = function(func) {
-    var funcString = func.toString();
-    var matches = funcString.match(/^function\s+?.*?\((.*?)\)/);
-    var argsNames = matches[1].split(',').map(function (arg) { return arg.trim(); });
-    var dependencies = this.dependencies;
+  var DI = (function(name, dependencies) {
     var self = this;
+    self.registeredFuncs = {};
 
-    // console.log('fs', funcString);
-    // console.log('ms', matches);
-    // console.log('aN', argsNames);
-    // console.log('ds', dependencies);
+    var module = {
+      name: name,
+      dependencies: dependencies,
+      register: register,
+      inject: inject,
+      getRegisteredFunc: getRegisteredFunc
+    }
 
-    return function () {
-      var cachedFuncs = [];
-
-      argsNames.map(function(el) {
-        if (el) {
-          cachedFuncs.push(self.registeredFuncs[el]);
-        }
-      });
-
-      return func.apply(this, cachedFuncs); ///.apply(this, args);
+    function register(name, func) {
+      self.registeredFuncs[name] = func;
     };
-  };
+
+    function inject(func) {
+      var funcString = func.toString();
+      var matches = funcString.match(/^function\s+?.*?\((.*?)\)/);
+      var argsNames = matches[1].split(',').map(function (arg) { return arg.trim(); });
+      // var dependencies = this.dependencies;
+      // var self = this;
+
+      // console.log('fs', funcString);
+      // console.log('ms', matches);
+      // console.log('aN', argsNames);
+      console.log('func', func);
+      console.log('ds', dependencies);
+
+      return function () {
+        var cachedFuncs = [];
+
+        argsNames.map(function(el) {
+          if (el) {
+            cachedFuncs.push(self.registeredFuncs[el]);
+          }
+        });
+
+        return func.apply(this, cachedFuncs); ///.apply(this, args);
+      };
+    };
+
+    function getRegisteredFunc(func) {
+      return this.registeredFuncs;
+    };
+
+    return module;
+  });
 
   module.exports = {
     modules: {},
