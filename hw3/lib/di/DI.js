32c11e61e9e2b466872223f96da9fdf20d0aa51f
Nina Baculinao <nb2406@columbia.edu>
2015-02-24 10:49:39 -0500

    test(di.js): add comments and attempts for last two tests, 16/18 passing

--- hw3/lib/di/DI.js
+++ hw3/lib/di/DI.js
@@ -1,34 +1,37 @@
 (function () {
   'use strict';
 
-  var DI = (function(name, dependencies) {
+  var DI = function(name, dependencies) {
     var self = this;
     self.registeredFuncs = {};
 
-    var module = {
+    self.module = {
       name: name,
       dependencies: dependencies,
       register: register,
       inject: inject,
       getRegisteredFunc: getRegisteredFunc
-    }
+    };
 
     function register(name, func) {
       self.registeredFuncs[name] = func;
-    };
+    }
 
     function inject(func) {
       var funcString = func.toString();
       var matches = funcString.match(/^function\s+?.*?\((.*?)\)/);
       var argsNames = matches[1].split(',').map(function (arg) { return arg.trim(); });
-      // var dependencies = this.dependencies;
-      // var self = this;
 
       // console.log('fs', funcString);
       // console.log('ms', matches);
       // console.log('aN', argsNames);
-      console.log('func', func);
-      console.log('ds', dependencies);
+      // console.log('func', func);
+      console.log('logging from within DI object...');
+      console.log('dependencies', dependencies);
+      console.log('this.modules', this.modules);
+
+      // for (var i in this.dependencies) {
+      // }
 
       return function () {
         var cachedFuncs = [];
@@ -41,27 +44,34 @@
 
         return func.apply(this, cachedFuncs); ///.apply(this, args);
       };
-    };
+    }
 
     function getRegisteredFunc(func) {
-      return this.registeredFuncs;
-    };
+      return self.registeredFuncs;
+    }
 
-    return module;
-  });
+    return self.module;
+  }; // wrapping non-IIFE function literals in parens is unnecessary?
 
   module.exports = {
     modules: {},
     module: function (name, dependencies) {
+      console.log('module function in exports getting called for arguments', arguments);
       if (arguments.length === 1) {
         for (var i = 0; i < this.modules.length; i++) {
           if (this.modules[i].name === name) {
+            console.log('this.modules in exports', this.module[i]);
             return this.module[i];
           }
         }
         // throw 'DI', name, 'is not available';
       } else {
+        // console.log('it always goes here to the else');
+        console.log('this.module[' + dependencies + ']', this.module[dependencies]);
+        console.log('creating new DI object...', name, dependencies);
         var module = new DI(name, dependencies);
+        this.modules[name] = module;
+        console.log('this.modules[' + name + ']=', this.modules[name]);
         return module;
       }
     }
